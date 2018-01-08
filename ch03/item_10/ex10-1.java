2e00dd646716a7eac03bd9adddd1595535388d66
FreeBz <freebz@hananet.net>
2018-01-08 13:44:48 +0900

    규칙10 toString은 항상 재정의하라

---
+++ ch03/item_10/ex10-1.java
@@ -0,0 +1,1 @@
+System.out.println("Failed to connect: " + phoneNumber);
---
+++ ch03/item_10/ex10-3.java
@@ -0,0 +1,8 @@
+/**
+ * 물약(potion)의 내용을 간단히 설명하는 문자열을 반환한다.
+ * 정해진 문자열 형식은 없으며, 바뀔 수 있다.
+ * 하지만 대체로 아래와 같은 문자열이 반환된다.
+ *
+ * "[Potion #9: type=love, smell=turpentine, look=india ink]"
+ */
+@Override public String toString() { ... }
