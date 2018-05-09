b29173ac6e05ae97b56f48bf7426f6d92d7a5518
FreeBz <freebz@hananet.net>
2018-05-09 13:53:15 +0900

    규칙 52 객체를 참조할 때는 그 인터페이스를 사용하라

---
+++ ch08/item_52/ex52-1.java
@@ -0,0 +1,9 @@
+// 인터페이스를 자료형으로 사용하고 있는, 바람직한 예제
+List<Subscriber> subscribers = new Vector<Subscriber>();
+
+
+// 클래스를 자료형으로 사용하는, 나쁜 예제
+Vector<Subscriber> subscribers = new Vector<Subscriber>();
+
+
+List<Subscriber> subscribers = new ArrayList<Subscriber>();
