22e97ef285115ec5ef2bd9ec0acd639721c5bc07
FreeBz <freebz@hananet.net>
2018-05-14 20:21:28 +0900

    규칙 72 스레드 스케줄러에 의존하지 마라

---
+++ ch10/item_72/ex72-1.java
@@ -0,0 +1,22 @@
+// CountDownLatch의 잘못된 구현 사례 - 계속 바쁘게 대기한다!
+public class SlowCountDownLatch {
+    private int count;
+    public SlowCountDownLatch(int count) {
+	if (count < 0)
+	    throw new IllegalArgumentException(count + " < 0");
+	this.count = count;
+    }
+
+    public void await() {
+	while (true) {
+	    synchronized(this) {
+		if (count == 0) return;
+	    }
+	}
+    }
+
+    public synchronized void countDown() {
+	if (count != 0)
+	    count--;
+    }
+}
