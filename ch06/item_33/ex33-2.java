6d7cc63f03f5a7fa6a8fdcf67c8e070b8b78333b
freebz <freebz@hananet.net>
2019-07-30 09:24:50 +0900

    규칙 33 ordinal을 배열 첨자로 사용하는 대신 EnumMap을 이용하라

--- ch06/item_33/ex33-2.java
+++ ch06/item_33/ex33-2.java
@@ -1,7 +1,7 @@
 // ordinal() 값을 배열 첨자로 사용 - 이러시면 곤란합니다
 Herb[] garden = ...;
 
-Set<Herb>[] herbsByType = // Indexed by Herb.Type.ordinal()
+Set<Herb>[] herbsByTYpe = // Indexed by Herb.Type.ordinal()
     (Set<Herb>[]) new Set[Herb.Type.values().length];
 for (int i = 0; i < herbsByType.length; i++)
     herbsByType[i] = new HashSet<Herb>();
